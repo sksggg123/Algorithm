19f05c8b2b0c525593b6d06441c76f2ba8af11ea
권병윤 <sksggg123@gmail.com>
2019-05-07 23:35:03 +0900

    백준 15652 문제 풀이 시작

---
+++ src/main/java/algorithm/Baekjoon_15652.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : algorithm
+ * create date  : 2019-05-06 23:00
+ */
+public class Baekjoon_15652 {
+    public static void main(String[] args) {
+
+    }
+}
